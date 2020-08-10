c3af4b3f2fd33e9c98709d0dc95da2eb00d50fd3
Geetanjali Kore <geetakore04@gmail.com>
2020-08-10 22:13:30 +0530

    Added small trtromino,previous tetromino and freeze function

--- js/index.js
+++ js/index.js
@@ -1,9 +1,26 @@
 document.addEventListener('DOMContentLoaded',()=>{
     const grid=document.querySelector('.grid');
+    const displaySquares=document.querySelectorAll('previous-grid div')
     let squares=Array.from(grid.querySelectorAll('div'))
     const width=10;
     const height=20;
     let currentPosotion=4;
+    let timerId
+
+    //assign functions to keycodes
+    function control(e){
+        if(e.keyCode===39){
+            moveRight()
+        }else if(e.keyCode===38){
+            rotate()
+        }else if(e.keyCode===37){
+            moveLeft()
+        }else if(e.keyCode===40){
+            moveDown()
+        }
+    }
+
+    document.addEventListener('keyup',control);
 
     //The Tetromonoes
     const lTetromino=[
@@ -90,5 +107,63 @@ document.addEventListener('DOMContentLoaded',()=>{
         draw()
     }
     
+    function rotate(){
+        undraw()
+        currentRotation++;
+        if(currentRotation===current.length){
+            currentRotation=0
+        }
+        current=theTetrominoes[random][currentRotation]
+        draw()
+    }
+    
+    //show previous tetromino is displaySquares
+    const displayWidth=4
+    const displayIndex=0
+    let nextRandom=0
+
+    const smallTetrominoes=[
+        [1,displayWidth+1,displayWidth*2+1,2],
+        [0,displayWidth,displayWidth+1,displayWidth*2+1],
+        [1,displayWidth,displayWidth+1,displayWidth+2],
+        [0,1,displayWidth,displayWidth+1],
+        [1,displayWidth+1,displayWidth*2+1,displayWidth*3+1]
+    ];
+
+    function displayShape(){
+        displaySquares.forEach(square=>{
+            square.classList.remove('block')
+    })
+   
+    smallTetrominoes[nextRandom].forEach(index=>{
+        displaySquares[displayIndex+index].classList.add('block')
+    })
+    
+}
+ //Freze the shape
+ function freez(){
+     if(current.some(index=>squares[currentPosotion + index +width].classList.contains('block3')) 
+     || squares[currentPosotion+index + width].classList.contains('block2')){
+         current.forEach(index =>squares[index+currentPosition].classList.add('block2'))
+             random=nextRandom
+             nextRandom=Math.floor(Math.random()*theTetrominoes.length)
+             current=theTetrominoes[random][currentRotation]
+             currentPosition=4
+             draw()
+             displayShape()
+         }
+     }
+ }
+
+     startBtn.addEventListener('click',()=>{
+         if(timerId){
+            clearInterval(timerId)
+            timerId=null
+         }else{
+             draw()
+             timerId=setInterval(moveDown,1000;)
+             nextRandom=Math.floor(Math.random()*theTetrominoes.length)
+         }
+     })
 
 })
