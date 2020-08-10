da352d031c662852d837dd179f36bace70c75975
Geetanjali Kore <geetakore04@gmail.com>
2020-08-10 12:07:05 +0530

    first commit of adding html div tags and css and tetrimonies in js file.

---
+++ js/index.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const grid=document.querySelector('.grid');
+    const width=10;
+    const height=20;
+
+    //The Tetromonoes
+    const lTetromino=[
+        [1,width+1,width*2+1,2],
+        [width,width+1,width+2,width*2+2],
+        [1,width+1,width*2+1,width*2],
+        [width,width*2,width*2+1,width*2+2]
+    ]
+
+    const zTetromino=[
+        [0,width,width+1,width*2+1],
+        [width+1,width+2,width*2,width*2+1],
+        [0,width,width+1,width*2+1],
+        [width+1,width+2,width*2,width*2+1]
+    ]
+
+    const tTetromino=[
+        [1,width,width+1,width+2],
+        [1,width+1,width+2,width*2+1],
+        [width,width+1,width*2,width*2+1],
+        [1,width,width+1,width*2+1]
+    ]
+
+    const oTetromino=[
+        [1,1,width,width+1],
+        [1,1,width,width+1],
+        [1,1,width,width+1],
+        [1,1,width,width+1]
+    ]
+
+    const iTetromino=[
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3],
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3]
+    ]
+
+    const theTetrominoes=[lTetromino,zTetromino,tTetromino,oTetromino,iTetromino]
+
+    //Rendomly select Tetromino
+    let random=Math.floor(Math.random()*theTetrominoes.length);
+    let currentRotation=0
+    let current=theTetrominoes[random][currentRotation]
+})
